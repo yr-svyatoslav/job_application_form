f2a936f65d5eabb5edd90d1982083841b2f72fd9
yt-svyatoslav <kramp9801@gmail.com>
2025-02-08 18:31:11 +0500

    Сделан контейнер, добавлена инструкция, создан класс для поднятия бэка

---
+++ src/main/java/com/project/job_application_form/CorsConfig.java
@@ -0,0 +1,23 @@
+package com.project.job_application_form;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:3000") // Укажите URL фронта
+                        .allowedMethods("GET", "POST", "PUT", "DELETE")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
